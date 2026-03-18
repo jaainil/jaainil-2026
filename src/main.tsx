0c6ada4f53aab4124ff74af1efd230cac5457a98
Jainil Prajapati <github-jainil@1209.in>
2026-03-18 17:29:39 +0530

    logo and fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import '@fontsource-variable/inter';
+import '@fontsource-variable/space-grotesk';
+import '@fontsource-variable/jetbrains-mono';
 import App from './App.tsx';
 import './index.css';
 
