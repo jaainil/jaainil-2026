8119de4d43ecffddf659c683463fdbdde53be301
Jainil Prajapati <github-jainil@1209.in>
2026-03-20 02:49:00 +0530

    fixed image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import keystatic from '@keystatic/astro';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), keystatic()],
   output: 'static',
   site: 'https://shravonix.com',
   vite: {
