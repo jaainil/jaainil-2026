c7375f233a760f826631385070a18d0d9be19d80
Jainil Prajapati <github-jainil@1209.in>
2026-03-20 02:37:27 +0530

    converted mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), mdx()],
   output: 'static',
   site: 'https://shravonix.com',
   vite: {
